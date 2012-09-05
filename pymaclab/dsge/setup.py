9af7cdda2c7644e73776bcc53bbaf156a5dea850
Eric M. Scheffel <emscheffel@gmail.com>
2012-09-06 02:45:06 +0800

    Modified on setup script in pymaclab/dsge for David Pugh who is on a Mac

--- pymaclab/dsge/setup.py
+++ pymaclab/dsge/setup.py
@@ -3,7 +3,6 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('dsge', parent_package, top_path)
-    config.add_data_dir('tests')
     config.add_subpackage('solvers')
     config.add_subpackage('parsers')
     return config
